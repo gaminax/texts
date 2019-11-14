0362f08f01d6ba6d2db7d348d47b8caae022e4ee
gaminax <6241468+gaminax@users.noreply.github.com>
2019-11-14 07:19:36 -0500

    url: texts

--- _sh/gendocs.sh
+++ _sh/gendocs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ -f gendocs.sh ] ; then cd ..; fi
 if [ ! -d _site ] ; then echo no _site; exit 1; fi
+if [ ! -d docs ] ; then echo no docs/ exit 1; fi
 cp -p _site/index.html docs
 for d in Josephus0090Antiquities Josephus0090War Josephus0090Life; do
     if [ ! -d docs/$d ] ; then mkdir docs/$d; fi
