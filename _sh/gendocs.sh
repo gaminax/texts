ef119a2cdc876c2320ae155aca9b9093249543ee
gaminax <6241468+gaminax@users.noreply.github.com>
2019-11-14 12:29:14 -0500

    add life_ad.ad

--- _sh/gendocs.sh
+++ _sh/gendocs.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-if [ -f gendocs.sh ] ; then cd ..; fi
-if [ ! -d _site ] ; then echo no _site; exit 1; fi
-if [ ! -d docs ] ; then echo no docs/ exit 1; fi
-cp -p _site/index.html docs
-for d in Josephus0090Antiquities Josephus0090War Josephus0090Life; do
-    if [ ! -d docs/$d ] ; then mkdir docs/$d; fi
-    if [ ! -d docs/$d ] ; then echo No directory: docs/$d; exit 1; fi
-    cp -p _site/$d/*.html docs/$d
-done
+
+d_sh=`dirname $0`
+cd $d_sh/..
+# if [ -f gendocs.sh ] ; then cd ..; fi
+
+if [ -d docs ] ; then rm -r docs; fi
+jekyll build
+mv _site docs
+
+##
+# if [ ! -d _site ] ; then echo no _site; exit 1; fi
+# if [ ! -d docs ] ; then echo no docs/ exit 1; fi
+# cp -p _site/index.html docs
+# for d in Josephus0090Antiquities Josephus0090War Josephus0090Life; do
+#     if [ ! -d docs/$d ] ; then mkdir docs/$d; fi
+#     if [ ! -d docs/$d ] ; then echo No directory: docs/$d; exit 1; fi
+#     cp -p _site/$d/*.html docs/$d
+# done
